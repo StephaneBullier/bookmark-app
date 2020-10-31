04da71adaff1b79518df4e020f23cb32e0abc0ed
steph <sbullier@gmail.com>
2020-10-31 19:22:57 +0100

    Add App and ResourceItem components

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <resource-item
+        v-for="resource in storedResources"
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+        :key="resource.id"
+    ></resource-item>
+  </ul>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+import ResourceItem from "@/components/learning-resources/ResourceItem";
+export default {
+  name: "App",
+  components: {
+    ResourceItem
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: uuidv4(),
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://v3.vuejs.org/'
+        },
+        {
+          id: uuidv4(),
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://www.google.com/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/learning-resources/ResourceItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h2>{{title}}</h2>
+        <button>Delete</button>
+      </header>
+      <p>{{description}}</p>
+      <nav>
+        <a :href="link">View resource</a>
+      </nav>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ResourceItem",
+  props: ['title', 'description', 'link']
+}
+</script>
+
+<style scoped>
+
+</style>
